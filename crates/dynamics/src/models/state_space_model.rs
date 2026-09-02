221f38f8f3b0a810bd7c8bb68fbdb5fa72c84746
Ansh Gupta <anshgupta721@gmail.com>
2026-09-02 11:25:50 -0500

    ran cargo fmt

--- crates/dynamics/src/models/state_space_model.rs
+++ crates/dynamics/src/models/state_space_model.rs
@@ -1,6 +1,6 @@
 use nalgebra::{SMatrix, SVector};
 
-pub trait LTVSystem<const NX: usize, const NU: usize, const NY: usize>{
+pub trait LTVSystem<const NX: usize, const NU: usize, const NY: usize> {
     fn a(&self, t: f64) -> SMatrix<f64, NX, NX>;
     fn b(&self, t: f64) -> SMatrix<f64, NX, NU>;
     fn c(&self, t: f64) -> SMatrix<f64, NY, NX>;
@@ -13,15 +13,21 @@ pub struct StateSpace<S, const NX: usize, const NU: usize, const NY: usize> {
     system: S,
 }
 
-impl<S, const NX: usize, const NU: usize, const NY: usize> StateSpace<S, NX, NU, NY> 
-    where S: LTVSystem<NX, NU, NY>{
-    pub fn new(
-        system: S,
-    ) -> StateSpace<S, NX, NU, NY> {
+impl<S, const NX: usize, const NU: usize, const NY: usize> StateSpace<S, NX, NU, NY>
+where
+    S: LTVSystem<NX, NU, NY>,
+{
+    pub fn new(system: S) -> StateSpace<S, NX, NU, NY> {
         StateSpace { system }
     }
 
-    pub fn derivative(&self, t: f64, x: SVector<f64, NX>, u: SVector<f64, NU>, dx_prev: SVector<f64, NX>) -> SVector<f64, NX> {
+    pub fn derivative(
+        &self,
+        t: f64,
+        x: SVector<f64, NX>,
+        u: SVector<f64, NU>,
+        dx_prev: SVector<f64, NX>,
+    ) -> SVector<f64, NX> {
         dx_prev + self.system.a(t) * x + self.system.b(t) * u
     }
 
