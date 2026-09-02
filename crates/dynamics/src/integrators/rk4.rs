2c407c3ea3028df04e436d45d3a399651cc7f2b2
Ansh Gupta <anshgupta721@gmail.com>
2026-09-02 16:53:21 -0500

    ran cargo fmt

--- crates/dynamics/src/integrators/rk4.rs
+++ crates/dynamics/src/integrators/rk4.rs
@@ -1,9 +1,16 @@
 use nalgebra::SVector;
 
-pub fn rk4<const NX: usize, const NU: usize>(f: &dyn Fn(f64, SVector<f64, NX>, SVector<f64, NU>, SVector<f64, NX>) -> SVector<f64, NX>, t: f64, dt: f64, x: SVector<f64, NX>, u: SVector<f64, NU>, dx0: SVector<f64, NX>) -> SVector<f64, NX>{
+pub fn rk4<const NX: usize, const NU: usize>(
+    f: &dyn Fn(f64, SVector<f64, NX>, SVector<f64, NU>, SVector<f64, NX>) -> SVector<f64, NX>,
+    t: f64,
+    dt: f64,
+    x: SVector<f64, NX>,
+    u: SVector<f64, NU>,
+    dx0: SVector<f64, NX>,
+) -> SVector<f64, NX> {
     let k1 = dt * f(t, x, u, dx0);
-    let k2 = dt * f(t + dt/2.0, x + k1 / 2.0, u, dx0);
-    let k3 = dt * f(t + dt/2.0, x + k2 / 2.0, u, dx0);
+    let k2 = dt * f(t + dt / 2.0, x + k1 / 2.0, u, dx0);
+    let k3 = dt * f(t + dt / 2.0, x + k2 / 2.0, u, dx0);
     let k4 = dt * f(t + dt, x + k3, u, dx0);
-    x + (k1 + 2.0*k2 + 2.0*k3 + k4) / 6.0
-}   
+    x + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
+}
--- crates/dynamics/src/lib.rs
+++ crates/dynamics/src/lib.rs
@@ -1,2 +1,2 @@
-pub mod models;
 pub mod integrators;
+pub mod models;
