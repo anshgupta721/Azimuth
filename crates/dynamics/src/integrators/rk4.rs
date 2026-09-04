66686c3bf1f71d183ca59bbf9118bc8470363c6c
Ansh Gupta <anshgupta721@gmail.com>
2026-09-04 18:30:39 -0500

    cargo fmt ran

--- crates/dynamics/src/integrators/rk4.rs
+++ crates/dynamics/src/integrators/rk4.rs
@@ -5,7 +5,7 @@ pub fn rk4<const NX: usize, const NU: usize>(
     t: f64,
     dt: f64,
     x: SVector<f64, NX>,
-    u: SVector<f64, NU>
+    u: SVector<f64, NU>,
 ) -> SVector<f64, NX> {
     let k1 = dt * f(t, x, u);
     let k2 = dt * f(t + dt / 2.0, x + k1 / 2.0, u);
