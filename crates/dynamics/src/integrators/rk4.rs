6b16f29a739c70ba22110056ba7cc905810b418f
Ansh Gupta <anshgupta721@gmail.com>
2026-09-05 15:47:33 -0500

    got rid of control vector when stepping rk4

--- crates/dynamics/src/integrators/rk4.rs
+++ crates/dynamics/src/integrators/rk4.rs
@@ -1,15 +1,14 @@
 use nalgebra::SVector;
 
 pub fn rk4<const NX: usize, const NU: usize>(
-    f: &dyn Fn(f64, SVector<f64, NX>, SVector<f64, NU>) -> SVector<f64, NX>,
+    f: &dyn Fn(f64, SVector<f64, NX>) -> SVector<f64, NX>,
     t: f64,
     dt: f64,
     x: SVector<f64, NX>,
-    u: SVector<f64, NU>,
 ) -> SVector<f64, NX> {
-    let k1 = dt * f(t, x, u);
-    let k2 = dt * f(t + dt / 2.0, x + k1 / 2.0, u);
-    let k3 = dt * f(t + dt / 2.0, x + k2 / 2.0, u);
-    let k4 = dt * f(t + dt, x + k3, u);
+    let k1 = dt * f(t, x);
+    let k2 = dt * f(t + dt / 2.0, x + k1 / 2.0);
+    let k3 = dt * f(t + dt / 2.0, x + k2 / 2.0);
+    let k4 = dt * f(t + dt, x + k3);
     x + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0
 }
